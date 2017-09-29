d5118066cab7149e1354d8cc7307ac7243da67d1
Ling Yang <yangling@yahoo-inc.com>
2017-09-28 21:32:36 -0700

    use array instead of 2D array to improve performance

--- src/com/leetcode/problem/Problem253.java
+++ src/com/leetcode/problem/Problem253.java
@@ -10,34 +10,33 @@ import java.util.Stack;
  *
  * Notes: we have meeting start events and end events. we need a meeting room for each
  * start event and release a meeting room for each end events.
- * TODO: improve performance.
  */
 public class Problem253 {
     public int minMeetingRooms(Interval[] intervals) {
-        int[][]events = new int[intervals.length * 2][2];
-        for (int i = 0; i < intervals.length; i++) {
-            events[i * 2] = new int[]{intervals[i].start, 0};
-            events[i * 2 + 1] = new int[]{intervals[i].end, 1};
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        for (int i = 0; i< intervals.length; i++) {
+            start[i] = intervals[i].start;
+            end[i] = intervals[i].end;
         }
-        Arrays.sort(events, (event, another) -> {
-            if (event[0] == another[0]) {
-                return -1 * (event[1] - another[1]);
-            } else {
-                return event[0] - another[0];
-            }
-        });
-
-        int currentInUse = 0;
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int currentRoomsInUse = 0;
         int maxRoomInUse = 0;
-        for (int i = 0; i < events.length; i++) {
-            if (events[i][1] == 0) {
-                currentInUse++;
-                if (currentInUse > maxRoomInUse) {
-                    maxRoomInUse = currentInUse;
-                }
-            } else {
-                currentInUse--;
+        int i = 0, j = 0;
+        while(i < start.length) {
+            // consume one meeting room
+            while(i < start.length && start[i] < end[j]) {
+                currentRoomsInUse++;
+                i++;
             }
+            if (currentRoomsInUse > maxRoomInUse) {
+                maxRoomInUse = currentRoomsInUse;
+            }
+
+            // release one meeting room
+            j++;
+            currentRoomsInUse--;
         }
         return maxRoomInUse;
     }
