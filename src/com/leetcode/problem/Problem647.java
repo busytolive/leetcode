8383dff8aecc15e71bb7200750f35cee8e41ed16
Ling Yang <yangling@yahoo-inc.com>
2017-11-08 20:27:43 -0800

    find Palindromic

---
+++ src/com/leetcode/problem/Problem647.java
@@ -0,0 +1,27 @@
+package com.leetcode.problem;
+
+/**
+ * Palindromic Substrings
+ *
+ * Algorithm: start from each index and try to extend palindrome for both odd and even length.
+ */
+public class Problem647 {
+    public int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(s, i, i);
+            count += expand(s, i, i + 1);
+        }
+        return count;
+    }
+
+    private int expand(String s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+        }
+        return count;
+    }
+}
